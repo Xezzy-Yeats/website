7c38985dde04c2cc4fe5ed4cffb9ec1f61dc2f99
Xezzy Yeats <ii1700@gmail.com>
2017-03-02 18:43:42 +0100

    Add customized greeting

--- scripts/main.js
+++ scripts/main.js
@@ -12,3 +12,16 @@ myImage.onclick = function() {
 	myLicense.textContent = 'cc-by-nd-2.0';
     }
 }
+
+var myButton = document.querySelector('button');
+var myHeading = document.querySelector('h1');
+
+function setUserName() {
+    var myName = prompt('Please enter your name.');
+    localStorage.setItem('name', myName);
+    myHeading.textContent = 'Mozilla is cool, ' + myName;
+}
+
+myButton.onclick = function() {
+    setUserName();
+}
