fab93480748be98418783967a3b30e75d8da5a4a
Xezzy Yeats <ii1700@gmail.com>
2017-03-02 15:11:10 +0100

    Add an alternative home page image
    Switch home page image between a ground pangolin and a breaching whale
    by clicking on the picture. New JavaScript code is responsible for
    switching images, and their licensing information.

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,18 @@
 var myHeading = document.querySelector('h1');
 myHeading.textContent = 'Hello, World!';
+
+var myImage = document.querySelector('img');
+var myLicense = document.getElementById('license');
+myImage.onclick = function() {
+    var mySrc = myImage.getAttribute('src');
+    if (mySrc === 'images/breaching_whale.jpg') {
+	myImage.setAttribute('src','images/ground_pangolin.jpg');
+	myLicense.setAttribute('href', 'https://flic.kr/p/M6EyUH');
+	myLicense.textContent = 'cc-by-2.0';
+    } else {
+	myImage.setAttribute ('src', 'images/breaching_whale.jpg');
+	myLicense.setAttribute('href', 'https://creativecommons.org/licenses/by-nd/2.0/');
+	myLicense.textContent = 'cc-by-nd-2.0';
+    }
+}
+// ground pangolin licensing information: 
